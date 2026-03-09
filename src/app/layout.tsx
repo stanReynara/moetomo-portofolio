001a6c1b27c2d7a85874a3c5b5eae86f7c010edc
stanReynara <stanleyreynara@gmail.com>
2026-03-09 21:39:43 +0700

    changed from metadata (deprecated) to viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import type { Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -29,7 +30,10 @@ export const metadata: Metadata = {
       },
     ],
   },
-  themeColor: '#E91E63',
+};
+
+export const viewport: Viewport = {
+  themeColor: '#1ABC9C',
 };
 
 export default function RootLayout({
