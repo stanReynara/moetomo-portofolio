03798a7b60d324af1a63aba3626e65d837b0e8ee
stanReynara <stanleyreynara@gmail.com>
2026-03-14 18:48:59 +0700

    revert edge runtime change

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -3,6 +3,8 @@ import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
 
+export const runtime = "edge"; // Run this API route on the Edge for faster response times
+
 async function handler(request: NextRequest) {
   const { env } = await getCloudflareContext({ async: true });
   const auth = getAuth(env);
